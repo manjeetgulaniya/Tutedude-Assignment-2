7ae619a9aeff05ccbf97eba01edc375ef9431997
Manjeet Gulaniya <145897864+manjeetgulaniya@users.noreply.github.com>
2025-09-17 18:12:54 +0000

    Task done

---
+++ task1.py
@@ -0,0 +1,7 @@
+num = int (input("Enter a number\t"))
+
+if num%2==0:
+    print(f"{num} is an even number")
+
+elif num%2!=0:
+    print(f"{num} is an odd number")
---
+++ task2.py
@@ -0,0 +1,5 @@
+num=int(50)
+
+sum = (num*(num+1))/2
+
+print(f"The sum of numbers from 1 to 50 is: {sum}")
